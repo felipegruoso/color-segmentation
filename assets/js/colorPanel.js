55690177cbbb91771b653538faa45546e307ba57
Felipe Gadelha Ruoso <felipe.gruoso@gmail.com>
2015-10-02 18:18:08 +0000

    Adds color panel Script.

---
+++ assets/js/colorPanel.js
@@ -0,0 +1,29 @@
+showRange = function(input) {
+  var output_id = input.attr('name');
+  var parent    = input.closest('#control');
+
+  parent.find('#' + output_id).val(input.val());
+};
+
+updateRange = function(output) {
+  var input_id = output.attr('name');
+  var parent   = output.closest('#control');
+  var value    = output.val().replace(/\D/, "");
+
+  parent.find('#' + input_id).val(value);
+  output.val(value);
+};
+
+addColor = function() {
+  $.get('views/controls.html?' + Date.now(), function(data) {
+    $('#menu').append(data);
+  });
+};
+
+$(document).on('change', 'input[type="range"]', function() {
+  showRange($(this));
+});
+
+$(document).on('keyup', 'input[type="text"]', function() {
+  updateRange($(this));
+});
